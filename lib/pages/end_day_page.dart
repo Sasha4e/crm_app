77519fb54bff9a7b9b391bb359be34b0e6552afd
Sasha <click.ufo@gmail.com>
2024-02-15 10:51:47 +0200

    save

--- lib/pages/end_day_page.dart
+++ lib/pages/end_day_page.dart
@@ -19,7 +19,13 @@ class _EndDayState extends State<EndDay> {
   late FocusNode _textFieldFocusNode;
   bool startedWork = false;
   late Map<String, dynamic> startDayData = {};
-
+  List<dynamic> reports = [];
+  String formatSeconds(int seconds) {
+    int hours = seconds ~/ 3600; // Делим на 3600 (количество секунд в часе)
+    int minutes = (seconds % 3600) ~/
+        60; // Делим оставшиеся секунды на 60 (количество секунд в минуте)
+    return '$hours h. $minutes min.';
+  }
   @override
   void initState() {
     super.initState();
@@ -73,7 +79,8 @@ class _EndDayState extends State<EndDay> {
             startDayData = jsonResponse;
             startedWork = true;
           });
-          print(startDayData['data']['reports']);
+          reports = startDayData['data']['reports'];
+          print(reports);
         }
       } else {
         print(
@@ -90,37 +97,124 @@ class _EndDayState extends State<EndDay> {
     return Scaffold(
       appBar: AppBar(title: Text('End day')),
       body: SingleChildScrollView(
+        // always adds scroll to avoid overflows
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 40),
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    hint: Text('Select Task'),
-                    value: selectedProjectId,
-                    onChanged: (String? projectId) {
-                      setState(() {
-                        selectedProjectId = projectId!;
-                        isTaskSelected = true;
-                        _textFieldFocusNode.requestFocus();
-                      });
-                      print(selectedProjectId);
-                    },
-                    items:
-                        userProjects.map<DropdownMenuItem<String>>((project) {
-                      return DropdownMenuItem<String>(
-                        value: project['id'].toString(),
-                        child: Text(project['project']['name'] +
-                            ' / ' +
-                            project['name']),
-                      );
-                    }).toList(),
-                  ),
+                //ADDED TASKS
+                reports.length > 0
+                    ? Container(
+                        margin: EdgeInsets.only(bottom: 45),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(bottom: 5),
+                              child: Text(
+                                'Already added reports:',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                                child: Column(
+                              children: reports.map((item) {
+                                return Row(
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.only(bottom: 5),
+                                        padding: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.grey),
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text(
+                                                    '${item['task']['project_name']}  /  ${item['task']['name']}'),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Text(formatSeconds(
+                                                    item['seconds']))
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }).toList(), // Преобразуем Iterable в список виджетов
+                            )),
+                          ],
+                        ),
+                      )
+                    : Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: CircularProgressIndicator(),
+                      ),
+
+                //SELECT
+                Column(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(bottom: 5),
+                        child: Text(
+                          'Add new report:',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 40),
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 0), // Устанавливаем границу
+                        ),
+                        child: DropdownButton<String>(
+                          isExpanded: true,
+                          hint: Text('Select Task'),
+                          value: selectedProjectId,
+                          onChanged: (String? projectId) {
+                            setState(() {
+                              selectedProjectId = projectId!;
+                              isTaskSelected = true;
+                              _textFieldFocusNode.requestFocus();
+                            });
+                            print(selectedProjectId);
+                          },
+                          items: userProjects
+                              .map<DropdownMenuItem<String>>((project) {
+                            return DropdownMenuItem<String>(
+                              value: project['id'].toString(),
+                              child: Text(project['project']['name'] +
+                                  ' / ' +
+                                  project['name']),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
+
+              
+
+
+
+
+
                 Visibility(
                   visible: isTaskSelected,
                   child: Column(
@@ -133,7 +227,7 @@ class _EndDayState extends State<EndDay> {
                           maxLines: 1,
                           keyboardType: TextInputType.number,
                           decoration: InputDecoration(
-                            hintText: 'Time',
+                            hintText: 'Hours',
                             border: OutlineInputBorder(),
                           ),
                         ),
